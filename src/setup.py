3a368fb9324312354a02b6829c8c4875a2bbfe69
Kibarski Jonatan <phenixdoc@gmail.com>
2013-02-17 12:17:36 +0200

    Changed package name to lowercase (breaks competability); added setup script

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PythonTools',
+		version='1.0',
+		description='Python Utilities',
+		author='Kibarski Jonathan',
+		author_email='phenixdoc@gmail.com',
+		packages=['pythontools']
+     )
