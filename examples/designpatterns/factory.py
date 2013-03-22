7b9a3209380eba311313df363c659aaf3074fed4
Kibarski Jonathan <phenixdoc@gmail.com>
2013-03-22 11:04:53 +0200

    added design pattern factory

---
+++ examples/designpatterns/factory.py
@@ -0,0 +1,24 @@
+from pythontools.designpatterns import factory
+import re
+
+class A:
+    def __init__(self, arg):
+        print "init A with %s" % arg
+    @staticmethod
+    def accept(arg):
+        return isinstance(arg, basestring) and re.match(r"^A", arg)
+factory.register(A)
+
+class B:
+    def __init__(self, arg):
+        print "init B with %s" % arg
+    @staticmethod
+    def accept(arg):
+        return isinstance(arg, basestring) and re.match(r"^B", arg)
+factory.register(B)
+
+if __name__ == "__main__":
+    obj1 = factory.new("Alice")
+    obj2 = factory.new("Bob")
+    print obj1
+    print obj2
---
+++ pythontools/designpatterns/factory.py
@@ -0,0 +1,11 @@
+from pythontools.mycollections import OrderedSet
+
+_classes = OrderedSet()
+
+def register(_class):
+    _classes.add(_class)
+
+def new(arg):
+    for _class in _classes:
+        if _class.accept(arg):
+            return _class(arg)
