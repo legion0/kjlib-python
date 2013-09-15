3a57650c26abeb27b4bbef8d6633cb4581601657
Kibarski Jonathan <phenixdoc@gmail.com>
2013-09-15 12:21:24 +0200

    moved to setuptools, added optional dep jsbeautifier

--- kjlib/__init__.py
+++ kjlib/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "1.06.01"
+__version__ = "1.06.02"
 
 def require(version):
 	currentVersion = __version__
--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
+'''
+Created on Feb 18, 2013
 
-from distutils.core import setup
+@author: legion
+'''
+from setuptools import setup
 from kjlib import __version__
 
-setup(name='kjlib',
-      version=__version__,
-      description='Reusable Python Modules',
-      author='K Jonathan',
-      author_email='phenixdoc@gmail.com',
-      packages=['kjlib'],
-	  url=r"https://github.com/legion0/kjlib-python"
-     )
+setup(
+	name = "kjlib",
+    version=__version__,
+	description='Reusable Python Modules',
+	packages=['kjlib'],
+	url=r"https://github.com/legion0/kjlib-python",
+
+	extras_require = {
+		'jsbeautifier':  ["jsbeautifier"],
+	},
+
+	author='K Jonathan',
+	author_email='phenixdoc@gmail.com',
+)
+
