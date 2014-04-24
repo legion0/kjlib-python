0ba3a1b93bc194be8fd226ba4a3f624070f7e7a5
legion0 <phenixdoc@gmail.com>
2014-04-24 10:59:33 +0300

    Update factory.py

--- kjlib/designpatterns/factory.py
+++ kjlib/designpatterns/factory.py
@@ -3,9 +3,10 @@ from kjlib.mycollections import OrderedSet
 _classes = OrderedSet()
 
 def register(class_):
-    _classes.add(class_)
+	_classes.add(class_)
 
-def new(arg):
-    for class_ in _classes:
-        if class_.accept(arg):
-            return class_(arg)
+def new(name, *args, **kwargs):
+	for class_ in _classes:
+		if class_.accept(name):
+			return class_(*args, **kwargs)
+	return None
