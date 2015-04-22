5a19d99dffba12407df2d6346c031205015485bf
legion0 <phenixdoc@gmail.com>
2015-04-22 07:33:33 +0300

    update main.py template

--- code_templates/main.py
+++ code_templates/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
 
-#import libs
-#libs.check()
-#import kjlib
-#kjlib.require("3.0.0")
+import libs
+libs.check()
+import kjlib
+kjlib.require("3.0.0")
 
 import argparse
-import os
 import json
+import os
 
 from kjlib.app_dirs import AppDirs
 from kjlib.logger import Logger
@@ -20,6 +20,7 @@ _CONF_DIR = _app_dirs.config()
 AppDirs.mkdir(_CONF_DIR)
 
 _CONF_FILE = os.path.join(_CONF_DIR, "config.json")
+_DEFAULT_CONFIG = {}
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Application Description')
@@ -31,16 +32,24 @@ def parse_args():
 
 	return args
 
-def main():
-	global _config, _datastore
+def _load_config():
+	config = {}
+	if not os.path.exists(_CONF_FILE):
+		config = _DEFAULT_CONFIG
+		with open(_CONF_FILE, "w") as f:
+			f.write(json.dumps(config, indent=4))
+	else:
+		with open(_CONF_FILE) as f:
+			config = json.load(open(_CONF_FILE))
+	return config
 
+def main():
 	args = parse_args()
 
 	# validate args
 	print args
 	exit(0)
 
-	_log.set_print_level(Logger.VERBOSE)
 	_config = json.load(open(_CONF_FILE))
 
 if __name__ == "__main__":
